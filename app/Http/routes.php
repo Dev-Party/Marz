c684b8738b5a2fe24582ffbb7cf1fa60eb374eb9
Felix Barros <soyFelixBarros@gmail.com>
2016-03-20 00:46:59 -0300

    Configuramos apiDoc para la documentacion

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,35 +1,51 @@
 <?php
 /**
  * @api {get} /radio Listar
- * @apiName  GetRadioAll
- * @apiGroup Radio
+ * @apiDescription  Ruta para obtener todas las radios.
+ * @apiName  All
+ * @apiGroup  Radio
+ * @apiPermission  none
  */
 $app->get('/radio', function () {});
 
 /**
  * @api {post} /radio Agregar
- * @apiName  PostRadioStore
- * @apiGroup Radio
+ * @apiDescription  Ruta para agregar una radio nueva.
+ * @apiName  Store
+ * @apiGroup  Radio
+ * @apiPermission  none
  */
 $app->post('/radio', function () {});
 
 /**
  * @api {get} /radio/:id Ver
- * @apiName  GetRadioShow
- * @apiGroup Radio
+ * @apiDescription  Ruta para ver la información de una radio.
+ * @apiName  Show
+ * @apiGroup  Radio
+ * @apiPermission  none
+ * 
+ * @apiParam {Number} id Identificador único de la radio.
  */
 $app->get('/radio/{id}', function () {});
 
 /**
  * @api {put} /radio/:id Actualiar
- * @apiName  PutRadioUpdate
- * @apiGroup Radio
+ * @apiDescription  Ruta para editar la información de una radio.
+ * @apiName  Update
+ * @apiGroup  Radio
+ * @apiPermission  admin
+ * 
+ * @apiParam {Number} id Identificador único de la radio.
  */
 $app->put('/radio/{id}', function () {});
 
 /**
  * @api {delete} /radio/:id Eliminar
- * @apiName  DeleteRadioDestroy
- * @apiGroup Radio
+ * @apiDescription  Ruta para eliminar una radio.
+ * @apiName  Destroy
+ * @apiGroup  Radio
+ * @apiPermission  admin
+ * 
+ * @apiParam {Number} id Identificador único de la radio.
  */
 $app->delete('/radio/{id}', function () {});
