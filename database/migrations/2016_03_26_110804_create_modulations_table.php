1747252a03766fb8208d3439e2c3ef05f9eaeb02
Felix Barros <soyFelixBarros@gmail.com>
2016-03-29 09:10:13 -0300

    Refactorizacion

--- database/migrations/2016_03_26_110804_create_modulations_table.php
+++ database/migrations/2016_03_26_110804_create_modulations_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateModulationTable extends Migration
+class CreateModulationsTable extends Migration
 {
     /** @var string $table Nombre de la tabla. */
     protected $table = 'modulations';
