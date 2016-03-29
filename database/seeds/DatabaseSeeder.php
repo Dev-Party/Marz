893391277018a9f4b18c5f5b96ecb5d97cb8eaa1
Felix Barros <soyFelixBarros@gmail.com>
2016-03-29 09:02:52 -0300

    Agregamos la tabla cities

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call('ModulationTableSeeder');
         $this->call('StatesTableSeeder');
+        $this->call('CitiesTableSeeder');
     }
 }
---
+++ tests/database/migrations/CitiesTableTest.php
@@ -0,0 +1,36 @@
+<?php
+
+class CitiesTableTest extends TestCase
+{
+    /** @var string $table Nombre de la tabla. */
+    protected $table = 'cities';
+
+    /** @var array $columns Nombres de los campos de una tabla. */
+    protected $columns = [
+        'id',
+        'name'
+    ];
+
+    /**
+     * Verificar la existencia de una tabla.
+     * 
+     * @return void
+     */
+    public function testCheckingForTable()
+    {
+        $this->assertTrue(Schema::hasTable($this->table));
+    }
+
+    /**
+     * Verificar si existen las columnas en un tabla.
+     *
+     * @return void
+     */
+    public function testCheckingForColumnsInATable()
+    {
+        for ($i = 0; count($this->columns) > $i; $i++) {
+            $this->assertTrue(Schema::hasColumn($this->table, $this->columns[$i]));
+        }
+    }
+
+}
