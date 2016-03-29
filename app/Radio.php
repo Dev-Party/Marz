e8b67f4b4e7eb6f32787d8b85190f5ab36e91533
Felix Barros <soyFelixBarros@gmail.com>
2016-03-29 03:48:44 -0300

    Protegimos el campo active

--- app/Radio.php
+++ app/Radio.php
@@ -11,14 +11,17 @@ class Radio extends Model
 
     /** @var array $fillable Los atributos que son asignables. */
     protected $fillable = [
-        'name',
         'modulation_id',
+        'name',
         'frequency',
         'streaming',
         'active',
         'created_at',
         'updated_at'
     ];
+
+    /** @var array $guarded Campos protegidos. */
+    protected $guarded = ['active'];
 
     /**
      * Obtener la modulación asociado a la radio.
