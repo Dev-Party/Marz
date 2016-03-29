09b0934be24e4315c4e94efa2a5e929d35e0bb06
Felix Barros <soyFelixBarros@gmail.com>
2016-03-29 08:17:42 -0300

    Creamos el modelo State

---
+++ app/State.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class State extends Model
+{
+    /** @var string $table Nombre de la tabla. */
+    protected $table = 'states';
+
+    /** @var array $fillable Los atributos que son asignables. */
+    protected $fillable = ['name'];
+
+    /** @var bool $timestamps Los campos timestamp de la tabla. */
+    public $timestamps = false;
+}
