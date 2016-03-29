5be54c8608340636cc91f360bc87965751e356e1
Felix Barros <soyFelixBarros@gmail.com>
2016-03-29 01:47:04 -0300

    Verificar la repuesta de modulation

---
+++ tests/app/Http/Controllers/ModulationsControllerTest.php
@@ -0,0 +1,23 @@
+<?php
+
+class ModulationsControllerTest extends TestCase
+{
+
+    /** @var array $values Valores para ingresar y comparar la tabla. */
+    protected $values = [
+        'name' => 'AM',
+        'name' => 'FM',
+        'name' => 'AM/FM'
+    ];
+
+    /**
+     * Verificar si la ruta radio retorna un objecto.
+     * 
+     *  @return void
+     */
+    public function testGetModulationAll()
+    {
+        $this->get('/modulation')
+             ->seeJson($this->values);
+    }
+}
