e90850376133a82ada3cb13c27b91d85f2f5855f
Felix Barros <soyFelixBarros@gmail.com>
2016-09-04 12:30:50 -0300

    Config db production

--- config/database.php
+++ config/database.php
@@ -1,19 +1,7 @@
 <?php
-return  [
-    'default' => 'pgsql',
-    'migrations' => 'migrations',
-    'connections' => [
-        'mysql' => [
-            'driver'    => 'mysql',
-            'host'      => env('DB_HOST', 'localhost'),
-            'database'  => env('DB_DATABASE', ''),
-            'username'  => env('DB_USERNAME', ''),
-            'password'  => env('DB_PASSWORD', ''),
-            'charset'   => 'utf8',
-            'collation' => 'utf8_unicode_ci',
-            'prefix'    => '',
-            'strict'    => false,
-        ],
+if (app()->environment('production')) {
+    return  [
+        'default' => 'pgsql',
         'pgsql' => [
             'driver'    => 'pgsql',
             'host'      => parse_url(env("DATABASE_URL"))["host"],
@@ -24,5 +12,5 @@ return  [
             'prefix'    => '',
             'schema'    => 'public',
         ],
-    ],
-];
+    ];
+}
