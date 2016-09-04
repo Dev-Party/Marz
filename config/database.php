8d7738bdef16bfc86fb53aaa4f95fa39296327aa
Felix Barros <soyFelixBarros@gmail.com>
2016-09-04 11:57:31 -0300

    Eliminamos el env heroku

---
+++ config/database.php
@@ -0,0 +1,28 @@
+<?php
+return  [
+    'default' => 'pgsql',
+    'migrations' => 'migrations',
+    'connections' => [
+        'mysql' => [
+            'driver'    => 'mysql',
+            'host'      => env('DB_HOST', 'localhost'),
+            'database'  => env('DB_DATABASE', ''),
+            'username'  => env('DB_USERNAME', ''),
+            'password'  => env('DB_PASSWORD', ''),
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+            'strict'    => false,
+        ],
+        'pgsql' => [
+            'driver'    => 'pgsql',
+            'host'      => env('DB_HOST', 'localhost'),
+            'database'  => env('DB_DATABASE', ''),
+            'username'  => env('DB_USERNAME', ''),
+            'password'  => env('DB_PASSWORD', ''),
+            'charset'   => 'utf8',
+            'prefix'    => '',
+            'schema'    => 'public',
+        ],
+    ],
+];
