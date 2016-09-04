d011e12240013de2c695ec0aaca05552f1b5cfda
Felix Barros <soyFelixBarros@gmail.com>
2016-09-04 12:42:18 -0300

    Agregamos la ruta de las migraciones

--- config/database.php
+++ config/database.php
@@ -1,18 +1,19 @@
 <?php
 if (!app()->environment('local')) {
-    return  [
-        'default' => 'pgsql',
-        'connections' => [
-            'pgsql' => [
-                'driver'    => 'pgsql',
-                'host'      => parse_url(env("DATABASE_URL"))["host"],
-                'database'  => substr(parse_url(env("DATABASE_URL"))["path"], 1),
-                'username'  => parse_url(env("DATABASE_URL"))["user"],
-                'password'  => parse_url(env("DATABASE_URL"))["pass"],
-                'charset'   => 'utf8',
-                'prefix'    => '',
-                'schema'    => 'public',
-            ],
+return  [
+    'default' => 'pgsql',
+    'migrations' => 'migrations',
+    'connections' => [
+        'pgsql' => [
+            'driver'    => 'pgsql',
+            'host'      => parse_url(env("DATABASE_URL"))["host"],
+            'database'  => substr(parse_url(env("DATABASE_URL"))["path"], 1),
+            'username'  => parse_url(env("DATABASE_URL"))["user"],
+            'password'  => parse_url(env("DATABASE_URL"))["pass"],
+            'charset'   => 'utf8',
+            'prefix'    => '',
+            'schema'    => 'public',
         ],
-    ];
+    ],
+];
 }
