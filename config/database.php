72f74975db918749e105d73b53cfcbdb17c1b3da
Felix Barros <soyFelixBarros@gmail.com>
2016-09-04 12:35:16 -0300

    Config db production4

--- config/database.php
+++ config/database.php
@@ -1,16 +1,30 @@
 <?php
 if (app()->environment('staging')) {
-    return  [
-        'default' => 'pgsql',
+return  [
+    'default' => 'pgsql',
+    'migrations' => 'migrations',
+    'connections' => [
+        'mysql' => [
+            'driver'    => 'mysql',
+            'host'      => env('DB_HOST', 'localhost'),
+            'database'  => env('DB_DATABASE', ''),
+            'username'  => env('DB_USERNAME', ''),
+            'password'  => env('DB_PASSWORD', ''),
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+            'strict'    => false,
+        ],
         'pgsql' => [
             'driver'    => 'pgsql',
-            'host'      => parse_url(getenv("DATABASE_URL"))["host"],
-            'database'  => substr(parse_url(getenv("DATABASE_URL"))["path"], 1),
-            'username'  => parse_url(getenv("DATABASE_URL"))["user"],
-            'password'  => parse_url(getenv("DATABASE_URL"))["pass"],
+            'host'      => parse_url(env("DATABASE_URL"))["host"],
+            'database'  => substr(parse_url(env("DATABASE_URL"))["path"], 1),
+            'username'  => parse_url(env("DATABASE_URL"))["user"],
+            'password'  => parse_url(env("DATABASE_URL"))["pass"],
             'charset'   => 'utf8',
             'prefix'    => '',
             'schema'    => 'public',
         ],
-    ];
+    ],
+];
 }
