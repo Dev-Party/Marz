06daa85094dac1156878af6c426b4b74aca61e23
Felix Barros <soyFelixBarros@gmail.com>
2016-10-01 01:32:37 -0300

    Agregamos las migraciones #2

---
+++ database/seeds/ModulationsTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ModulationsTableSeeder extends Seeder
+{
+      /**
+       * Ejecutar seed.
+       * 
+       * @return void
+       */
+      public function run()
+      {
+            DB::table('modulations')->insert([
+                  ['name' => 'AM'],
+                  ['name' => 'FM'],
+                  ['name' => 'AM/FM']
+            ]);
+      }
+}
