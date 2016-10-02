69d6f15e3d8089700221e0061ecd088dcb42175a
Felix Barros <soyFelixBarros@gmail.com>
2016-10-02 18:25:15 -0300

    Modificamos el nombre de la app

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'Laravel',
+    'name' => 'Marz v1.1.0',
 
     /*
     |--------------------------------------------------------------------------
