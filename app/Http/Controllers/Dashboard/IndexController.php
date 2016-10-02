64e3d072db161ee371bbab6e0902c17326b0a837
Felix Barros <soyFelixBarros@gmail.com>
2016-10-02 18:38:06 -0300

    Agregamos el método auth #9

--- app/Http/Controllers/Dashboard/IndexController.php
+++ app/Http/Controllers/Dashboard/IndexController.php
@@ -7,6 +7,15 @@ use App\Http\Controllers\Controller;
 
 class IndexController extends Controller
 {
+	/**
+     * Crear una nueva instancia de controlador.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
 
     /**
      * Metodo para manejar la página principal del Dashboard.
