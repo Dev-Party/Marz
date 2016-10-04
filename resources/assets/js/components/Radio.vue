d61a289dbe116fc0013487bc8e00175a3c5c04f6
Felix Barros <soyFelixBarros@gmail.com>
2016-10-04 12:13:08 -0300

    #12 - Reproductor de audio

--- resources/assets/js/components/Radio.vue
+++ resources/assets/js/components/Radio.vue
@@ -2,16 +2,20 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default" v-for="radio in radios | orderBy 'name' order">
-                    <div class="panel-body">
-                      <div class="media">
-                        <div class="media-body">
-                          <h4 class="media-heading">{{ radio.name }} - {{ radio.frequency }} Mhz</h4>
-                          <p>{{ radio.city }}</p>
-                        </div>
-                      </div>
+              <audio id="audio"></audio>
+              <div class="panel panel-default" v-for="radio in radios | orderBy 'name'">
+                <div class="panel-body">
+                  <div class="media">
+                    <div class="media-left">
+                      <a href="#" v-on:click="playRadio(radio.id, radio.streaming, $event)"><img id="i-{{ radio.id }}"class="media-object" src="http://marz.herokuapp.com/img/play.png"></a>
                     </div>
+                    <div class="media-body">
+                      <h4 class="media-heading">{{ radio.name }} {{ radio.frequency }} Mhz</h4>
+                      <span>{{ radio.city }}, {{ radio.state }}</span>
+                    </div>
+                  </div>
                 </div>
+              </div>
             </div>
         </div>
     </div>
@@ -21,9 +25,7 @@
 export default {
   data () {
     return {
-      city: '',
       order: 1,
-      playing: false,
       radios: []
     }
   },
@@ -39,6 +41,21 @@ export default {
       }, function (response) {
         console.log(response.status);
       });
+    },
+    playRadio: function (id, streaming, event) {
+      if (event) event.preventDefault()
+
+      var audio = document.getElementById('audio');
+
+      if (audio.paused) {
+        document.getElementById("i-" + id).src = "http://marz.herokuapp.com/pause.png";
+        audio.src = streaming + "/;stream/1";
+        audio.play();
+      } else {
+        document.getElementById("i-" + id).src = "http://marz.herokuapp.com/play.png";
+        audio.pause();
+      }
+
     }
   }
 }
