82c25078c0d96e8c88fb9d4a8dccce167a045802
Felix Barros <soyFelixBarros@gmail.com>
2016-10-11 01:30:48 -0300

    Refactorizacion plus

--- resources/assets/js/components/radio/List.vue
+++ resources/assets/js/components/radio/List.vue
@@ -23,7 +23,7 @@
         </audio>
       </div>
       <div class="media-body">
-        <h4 class="media-heading">{{ radio.name }} {{ radio.frequency }} Mhz</h4>
+        <h4 class="media-heading">{{ radio.modulation }} {{ radio.name }} {{ radio.frequency }} Mhz.</h4>
         <span>{{ radio.city }}, {{ radio.state }}</span>
       </div>
     </div>
