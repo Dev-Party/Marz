a1da68d6f910d84844a97d223c465edf2b8596c3
Felix Barros <soyFelixBarros@gmail.com>
2016-10-31 12:23:57 -0300

    Refactorizacion

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -4,7 +4,7 @@ require('./bootstrap');
 Vue.config.debug = true;
 
 Vue.component('list-radio', require('./components/radio/List.vue'));
-Vue.component('create-radio', require('./components/radio/Create.vue'));
+Vue.component('form-radio', require('./components/radio/Form.vue'));
 Vue.component('table-radio', require('./components/dashboard/radio/Table.vue'));
 
 const app = new Vue({
