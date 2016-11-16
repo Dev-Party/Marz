3d3ddc470db82d3d259dcbe9539a2d5aa2c2cf63
Felix Barros <soyFelixBarros@gmail.com>
2016-11-16 10:31:37 -0300

    View settings index #53

---
+++ resources/views/account/settings/index.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h3 class="panel-title">Ajustes</h3>
+				</div><!-- .panel-heading -->
+
+				<div class="panel-body">
+					
+				</div><!-- .panel-body -->
+			</div><!-- .panel -->
+		</div>
+        </div>
+    </div>
+</div>
+@endsection
