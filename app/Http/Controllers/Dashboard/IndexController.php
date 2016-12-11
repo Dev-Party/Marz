871e685967950ffff9f93352b89c50415b2ba366
Felix Barros <soyFelixBarros@gmail.com>
2016-12-11 04:33:12 -0300

    Refactorizacion #62

--- app/Http/Controllers/Dashboard/IndexController.php
+++ app/Http/Controllers/Dashboard/IndexController.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers\Dashboard;
 
 class IndexController extends Controller
 {
-	public function getRadios()
-	{
-		return view('dashboard.radios.all');
-	}
+	 public function getRadios()
+	 {
+		 return view('dashboard.radios.all');
+	 }
 }
