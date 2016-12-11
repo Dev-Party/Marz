024b90eb753d51a5b5f2b371fc3f50df5efde8bb
Felix Barros <soyFelixBarros@gmail.com>
2016-12-11 05:16:36 -0300

    Refactorizacion #62

--- database/seeds/ModulationsTableSeeder.php
+++ database/seeds/ModulationsTableSeeder.php
@@ -4,17 +4,17 @@ use Illuminate\Database\Seeder;
 
 class ModulationsTableSeeder extends Seeder
 {
-      /**
-      * Ejecutar seed.
-      *
-      *
-      * @return void
-      */
-      public function run()
-      {
-            DB::table('modulations')->insert([
-                  ['name' => 'AM'],
-                  ['name' => 'FM'],
-            ]);
-      }
+	/**
+	* Ejecutar seed.
+	*
+	*
+	* @return void
+	*/
+	public function run()
+	{
+		DB::table('modulations')->insert([
+			['name' => 'AM'],
+			['name' => 'FM'],
+		]);
+	}
 }
